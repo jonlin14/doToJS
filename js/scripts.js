5f6fc5ed9e5f629f93aa5ec7b17536738cc846d3
Jonathan Lin & Patrick Fiorentino <student@epicodus.com>
2015-04-16 16:55:26 -0700

    fixed bugs

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
-$(function () {
+// $(function () {
+  $(document).ready(function (){
+
 
     var selectedList;
     // var new_List = { name,  tasks: []};
@@ -11,33 +13,38 @@ $(function () {
         $('#lists').append('<li class="list">' + new_List.name + '</li>');
         //Attaching a event handler to every list class made to make selectedList equal
         //to the new_List made. We can now run our code on selectedList
+          selectedList = new_List;
         $('.list').last().click(function() {
           selectedList = new_List;
           //display all tasks for selected list
-          $('#tasks').append(selectedList.tasks);
+          // $('#tasks').append(selectedList.tasks);
+          $("ul#tasks").text("");
+          selectedList.tasks.forEach(function(task) {
+           $("ul#tasks").append("<li>" + task.name + "</li>");
+          });
         });
-    });
 
-    $('form#new_Task').submit(function(event) {
-        event.preventDefault();
-        var new_Task_Name = $('input#task_Name').val();
-        //var new_Task_Description = $('input#task_Description').val();
-        var new_Task = { name: new_Task_Name};
-        console.log(selectedList);
-        selectedList.tasks.push(new_Task)
-        // $('#tasks').append('<li class="task">' + new_Task.name + '</li>');
+        $('form#new_Task').unbind('submit');
+        $('form#new_Task').submit(function(event) {
+            event.preventDefault();
+            var new_Task_Name = $('input#task_Name').val();
+            //var new_Task_Description = $('input#task_Description').val();
+            var new_Task = { name: new_Task_Name};
+            console.log(selectedList);
+            selectedList.tasks.push(new_Task)
+            // $('#tasks').append('<li class="task">' + new_Task.name + '</li>');
 
 
-        //   $('#tasks').each(function() {
-        //     var new_Task = {name: $(this).find('input#.task_Name')};
-        //
-        //
-        // });
-
-         selectedList.tasks.forEach(function(task) {
-          $("ul#tasks").append("<li>" + task.name + "</li>");
-
-       });
+            //   $('#tasks').each(function() {
+            //     var new_Task = {name: $(this).find('input#.task_Name')};
+            //
+            //
+            // });
+            $("ul#tasks").text("");
+             selectedList.tasks.forEach(function(task) {
+              $("ul#tasks").append("<li>" + task.name + "</li>");
+            });
+        });
 
 
 
@@ -55,7 +62,7 @@ $(function () {
         // });
 
 
-    });
 
 
+  });
 });
