767f7bfd1a9d54705c10ca9bc18d9b294a6e361d
Jonathan Lin & Patrick Fiorentino <student@epicodus.com>
2015-04-16 14:40:48 -0700

    Work out some bugs

--- js/scripts.js
+++ js/scripts.js
@@ -7,9 +7,10 @@ $(function () {
         event.preventDefault();
         var new_List_Name = $('input#list_Name').val();
         var new_List = { name: new_List_Name, tasks: []};
-
+        //Adding a list class to lists id
         $('#lists').append('<li class="list">' + new_List.name + '</li>');
-
+        //Attaching a event handler to every list class made to make selectedList equal
+        //to the new_List made. We can now run our code on selectedList
         $('.list').last().click(function() {
           selectedList = new_List;
           //display all tasks for selected list
@@ -20,19 +21,29 @@ $(function () {
     $('form#new_Task').submit(function(event) {
         event.preventDefault();
         var new_Task_Name = $('input#task_Name').val();
-        // var new_Task_Description = $('input#task_Description').val();
-        new_List.name = $('#lists').last();
-
-
+        //var new_Task_Description = $('input#task_Description').val();
         var new_Task = { name: new_Task_Name};
-        new_List.tasks.push(new_Task);
-        console.log(new_List);
+        console.log(selectedList);
+        selectedList.tasks.push(new_Task)
+        // $('#tasks').append('<li class="task">' + new_Task.name + '</li>');
 
 
-        $('input#task_Name').val("");
-        $('input#task_Description').val("");
+        //   $('#tasks').each(function() {
+        //     var new_Task = {name: $(this).find('input#.task_Name')};
+        //
+        //
+        // });
 
-        $('#tasks').append('<li class="task">' + new_Task.name + '</li>');
+        selectedList.tasks.forEach(function(task) {
+          $("ul#tasks").text("<li>" + task.name + "</li>");
+
+        });
+
+
+
+
+
+
 
         // $('.task').last().click(function() {
         //     $(this).remove();
